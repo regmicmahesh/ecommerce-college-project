5eb7bff9d13b8c1c892a7b58d993d6892cdeb11b
Regmi C. Mahesh <0xmahadev@gmail.com>
2023-09-09 20:57:02 +0545

    initial commit

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
+import config from "../config";
 
-const supabase = createClient(
-    "https://mllvyyhfrytrctscstaq.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sbHZ5eWhmcnl0cmN0c2NzdGFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQyNDgyNjgsImV4cCI6MjAwOTgyNDI2OH0.k3rOKqYDrw5QmRQZ8bkEzozBGsK7xIcS8-oI5jCoqUw"
-);
+const supabase = createClient(config.supabaseUrl, config.supabaseKey);
 export default supabase;
